160f102d46a87603edace58adf1a55c6c7de2df7
ABRINI Mouad <45606320+AbrThecoder@users.noreply.github.com>
2019-10-20 15:51:57 +0200

    Quadrilatère (v0.2)

---
+++ Analyse5/Outils.py
@@ -0,0 +1,14 @@
+from sympy import diff, latex, Symbol
+
+#calcul gradient fonction n variables
+def grad(f,*argv):
+    arguments = []
+    for arg in argv:
+        arg = Symbol(str(arg))
+        arguments.append(arg)
+    scalaires = []
+    for i in arguments:
+        lat = latex(diff(str(f), i))
+        scalaires.append(lat)
+        print(lat)
+    return scalaires
--- Analyse5/Triangle.py
+++ Analyse5/Triangle.py
@@ -1,6 +1,6 @@
 # importation des modules
 from math import sqrt
-from sympy import diff, symbols, latex
+from Analyse5.Outils import grad
 
 
 # la fonction qui permet de calculer le périmètre d'un triangle
@@ -9,7 +9,6 @@ def P(a, b, c):
 
 
 # la fonction qui permet de calculer l'Aire d'un triangle (Héron), s est le demi-périmètre
-
 def A(a, b, c):
 	s = P(a, b, c) / 2
 	try:
@@ -18,15 +17,5 @@ def A(a, b, c):
 		return "Ce n'est pas un triangle"
 
 
-# calcul gradient fonction 3 variable
-def grad(f):
-	a, b, c, s = symbols('a b c s')
-	scalaires = []
-	for i in a, b, c:
-		lat = latex(diff(str(f), i))
-		scalaires.append(lat)
-		print(lat)
-	return scalaires
-
-
-print(grad("sqrt(s*(s-a)*(s-b)*(s-c))"))
+#calcul du grad d'une fonction de 3 variables
+print(grad("sqrt(s*(s-a)*(s-b)*(s-c))","a","b","c"))
